95535ff8fbc71f86d8e91672133ef91a6beb6052
Elvis Nzioki Makuthi <emakuthi@gmail.com>
2021-10-19 22:57:50 +0300

    added bottom sheet

--- lib/main.dart
+++ lib/main.dart
@@ -34,9 +34,12 @@ class _MainPageState extends State<MainPage> {
     ),
   );
   String delete = 'No Choice Made';
+  String location = 'No location chosen';
+  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -55,6 +58,13 @@ class _MainPageState extends State<MainPage> {
                   context: context,
                   builder: (context) {
                     return AlertDialog(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        side: const BorderSide(
+                          color: Colors.red,
+                          width: 2,
+                        ),
+                      ),
                       actions: [
                         TextButton(
                           onPressed: () {
@@ -83,7 +93,89 @@ class _MainPageState extends State<MainPage> {
               },
               child: const Text('Delete'),
             ),
-            Text(delete)
+            Text(delete),
+            ElevatedButton(
+              onPressed: () async {
+                final String loc = await showDialog(
+                  context: context,
+                  builder: (context) {
+                    return SimpleDialog(
+                      title: const Text('Choose Your Location'),
+                      children: [
+                        SimpleDialogOption(
+                          onPressed: () {
+                            Navigator.pop(context, 'Kenya');
+                          },
+                          child: const Text('Kenya'),
+                        ),
+                        SimpleDialogOption(
+                          onPressed: () {
+                            Navigator.pop(context, 'America');
+                          },
+                          child: const Text('America'),
+                        ),
+                        SimpleDialogOption(
+                          onPressed: () {
+                            Navigator.pop(context, 'Europe');
+                          },
+                          child: const Text('Europe'),
+                        )
+                      ],
+                    );
+                  },
+                );
+                setState(() {
+                  location = loc;
+                });
+              },
+              child: const Text('Choose Location'),
+            ),
+            Text(location),
+            ElevatedButton(
+              onPressed: () {
+                scaffoldKey.currentState?.showBottomSheet((context) {
+                  return Container(
+                    child: Center(
+                      child: Column(
+                        children: [
+                          const Text(
+                            'Are you sure you want to delete?',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
+                          ),
+                          ButtonBar(
+                            alignment: MainAxisAlignment.center,
+                            children: [
+                              TextButton(
+                                onPressed: () {},
+                                child: const Text(
+                                  'Yes',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 25,
+                                  ),
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    height: 110,
+                    decoration: const BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10),
+                      ),
+                    ),
+                  );
+                });
+              },
+              child: const Text('Bottom Sheet'),
+            )
           ],
         ),
       ),
